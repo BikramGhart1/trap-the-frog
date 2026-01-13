cf27dc79f56d3e5f3f604b238ed65bf7d3c0ca26
BikramGhart1 <coludingrain@gmail.com>
2026-01-13 17:09:47 +0545

    work on frontend logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Board from './components/Board'
 
 function App() {
+  const [moves, setMoves]=useState(0)
 
   return (
     <>
       <div className="app-container" >
         <h1 className="fs-5" style={{color:"#e0ffefff"}}>Trap The Frog</h1>
         <div className='d-flex flex-row justify-content-between'>
-          <p>Moves - <span>0</span></p>
+          <p>Moves - <span>{moves}</span></p>
           <div className='d-flex flex-row gap-4'>
           <p>Frog - <span>🐸</span></p>
           <p>Obstacle - <button style={{width:"15px", height:"15px", backgroundColor:"#23d160",borderRadius:"4px", border:"none", outline:"none"}}></button></p>
           </div>
         </div>
-        <Board/>
+        <Board setMoves={()=>setMoves((prev)=>prev+1)
+        }/>
         </div>      
     </>
   )
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 
 const GRID_SIZE = 12;
-const FROG_START = { row: 5, col: 8 };
 
-function Board() {
+function Board({setMoves}) {
   const [grid, setGrid] = useState(
     Array.from({ length: GRID_SIZE }, () =>
       Array.from({ length: GRID_SIZE }, () => false)
     )
   );
+  const [frogStart, setFrogStart]=useState({row: 5, col: 8 });
+  // const [isTrapped, setIsTrapped]=useState(false);
+  const directions=[
+      { dRow: -1, dCol: 0 }, // up
+  { dRow: 1, dCol: 0 },  // down
+  { dRow: 0, dCol: -1 }, // left
+  { dRow: 0, dCol: 1 },  // right
+  { dRow: -1, dCol: -1 }, // up-left
+  { dRow: -1, dCol: 1 },  // up-right
+  { dRow: 1, dCol: -1 },  // down-left
+  { dRow: 1, dCol: 1 },   // down-right
+  ];
+  // let FROG_START = { row: 5, col: 8 };
+
+  const canMoveTo=(row,col)=>{
+    console.log("can move: ",grid[row][col]);
+    if(grid[row][col]) return false;
+
+    return true;
+  }
 
   const toggleTile = (row, col) => {
     setGrid((prev) =>
@@ -17,7 +36,61 @@ function Board() {
         r.map((cell, cIdx) => (rIdx === row && cIdx === col ? !cell : cell))
       )
     );
+    // frogStart.row += 1;
+    // frogStart.col -= 1;
+    // setFrogStart((prev)=>({
+    //   row:prev.row+1,
+    //   col:prev.col-1,
+    // }))
   };
+
+  const gameOver=(row,col)=>{
+    return(
+      row === 0 ||
+      row === GRID_SIZE -1 ||
+      col === 0 ||
+      col === GRID_SIZE -1
+    );
+  };
+
+  const moveFrog=()=>{
+    const shuffledDirections=[...directions].sort(()=>Math.random()-0.5)
+    for (let dir of shuffledDirections){
+      const newRow=frogStart.row+dir.dRow;
+      const newCol=frogStart.col+dir.dCol;
+      if (canMoveTo(newRow, newCol)){
+        setFrogStart({row:newRow, col:newCol});
+        break;
+      }
+    }
+  }
+
+  const isTrapped=(row,col)=>{
+    for (let dir of directions){
+      const newRow=dir.dRow+row;
+      const newCol=dir.dCol+col;
+
+      if(newRow>=0 && newRow<GRID_SIZE && newCol>=0 && newCol<GRID_SIZE && grid[newRow][newCol]===false){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  useEffect(()=>{
+    moveFrog();
+  },[grid])
+
+  useEffect(()=>{
+    if(gameOver(frogStart.row, frogStart.col)){
+      alert("Frog escaped! You Lose.");
+      return;
+    }
+
+    if(isTrapped(frogStart.row, frogStart.col)){
+      alert("Frog Trapped! You win.")
+    }
+  },[frogStart]);
 
   return (
     <div
@@ -26,7 +99,7 @@ function Board() {
     >
       {grid.map((row, ridx) =>
         row.map((cell, cidx) => {
-          const isFrog = ridx === FROG_START.row && cidx === FROG_START.col;
+          const isFrog = ridx === frogStart.row && cidx === frogStart.col;
           return (
             <div
               key={`${ridx}-${cidx}`}
